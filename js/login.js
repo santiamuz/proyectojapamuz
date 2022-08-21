8f338da2f56a0b940dcca8ab69b5e291ac2dc5a7
santiamuz <santiamuz@outlook.com>
2022-08-21 19:38:31 -0300

    Se hizo parte 1 de ENTREGA 2
    Guardamos nombre de usuario en esquina superior derecha, haciendo uso de local storage

--- js/login.js
+++ js/login.js
@@ -1,3 +1,11 @@
+
 function ingresar(){
     location.replace("portada.html")
+    guardarEnLocalStorage();
 }
+
+function guardarEnLocalStorage(){
+let email = document.getElementById("email").value;
+localStorage.setItem("email", email);
+}
+
