e3a43b969baec52c3779d18bfe73505b4bf46fa7
santiamuz <santiamuz@outlook.com>
2022-10-05 21:15:23 -0300

    Puntos 1 y 2 de entrega 5

--- js/cart.js
+++ js/cart.js
@@ -1,2 +1,46 @@
+let cart_url = "https://japceibal.github.io/emercado-api/user_cart/25801.json"
+
 let email = localStorage.getItem("email");
 document.getElementById("usuario").innerHTML = email;
+
+let cartArray = [];
+
+document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("carrito").innerHTML= "";
+getJSONData(cart_url).then(function(resultObj){
+    if (resultObj.status === "ok"){
+        cartArray = resultObj.data;
+        showCart(cartArray);
+        }
+    }    
+)
+})
+
+function showCart (array){
+    let row = ""; 
+    
+
+
+    for(let i = 0; i < array.articles.length; i++){
+        let article = array.articles[i];
+      
+
+
+     row += ` 
+
+     <tr>
+     <td class="col" >  <img src= `+ article.image +`  id="artImg">  </td>
+        <td class = "col mr-3"> ` + article.name +` </td>
+        <td class = "col">  <strong> ` + article.unitCost +`</strong> </td>
+        <td> <input type = "number" value="1" id= "cantidad"> </td>
+        <td> <strong> ` + article.unitCost + `</strong> </td>
+     </tr>  
+     ` 
+        
+    
+    
+    document.getElementById("carrito").innerHTML = row;
+  }
+
+    
+}
--- js/product-info.js
+++ js/product-info.js
@@ -6,7 +6,7 @@ const enviar = document.getElementById("enviar");
 const comentario = document.getElementById("comentario");
 const contenedor = document.getElementById("comments");
 const puntuacion = document.getElementById("puntuacion");
-let today = new Date ();
+let today = new Date();
 let now = today.toLocaleString();
 
 let email = localStorage.getItem("email");
@@ -18,158 +18,161 @@ let productArray = [];
 
 
 
-document.addEventListener("DOMContentLoaded", function(e){
-    document.getElementById("product-info").innerHTML="";
-getJSONData(product_url).then(function(resultObj){
-    if (resultObj.status === "ok"){
-        productArray = resultObj.data;
-        showProduct(productArray);
+document.addEventListener("DOMContentLoaded", function (e) {
+    document.getElementById("product-info").innerHTML = "";
+    getJSONData(product_url).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            productArray = resultObj.data;
+            showProduct(productArray);
         }
-}    
-)
+    }
+    )
 })
 
 
 
 //Función que recibe el array del JSON y los muestra en pantalla usando DOM
-function showProduct(array){
+function showProduct(array) {
 
-let img = ""; 
-for (let i = 0; i < array.images.length; i++) {
-    img += ` <img src= " `+ array.images[i]+` " id = "images">` 
-} 
+    let img = "";
+    for (let i = 0; i < array.images.length; i++) {
+        img += ` <img src= " ` + array.images[i] + ` " id = "images">`
+    }
     let htmlContentToAppend = "";
+
+
+    htmlContentToAppend += ` 
     
-    
-            htmlContentToAppend += ` 
-    
-            
+            <div class= "container" >
                     <h2 class= "mt-3 pl-6"> `+ array.name + ` </h2>
 
                     <hr>
 
                     <h3 class = "ml-3"> Precio </h3> 
-                    <p> `+ array.currency + ` `+ array.cost + ` </p> 
+                    <p> `+ array.currency + ` ` + array.cost + ` </p> 
               
                     <h3> Descripción </h3> 
-                        <p> `+ array.description +`</p> 
+                        <p> `+ array.description + `</p> 
 
                     <h3> Categoría </h3> 
 
-                    <p> `+ array.category +`</p> 
+                    <p> `+ array.category + `</p> 
 
                     <h3> Cantidad vendidos </h3> 
                         
-                    <p> `+ array.soldCount +`</p>    
+                    <p> `+ array.soldCount + `</p>    
 
                     <h3> Imágenes ilustrativas </h3> 
-
-                    `+ img +` 
+            </div>
+                    `+ img + ` 
       
         `
-        document.getElementById("product-info").innerHTML = htmlContentToAppend;
-
-       
-    }
+    document.getElementById("product-info").innerHTML = htmlContentToAppend;
 
 
+}
 
- // Array donde cargar los comentarios 
+
+
+// Array donde cargar los comentarios 
 
 let commentsArray = [];
 
-document.addEventListener("DOMContentLoaded", function(e){
-    document.getElementById("comments").innerHTML="";
-getJSONData(comments_url).then(function(resultObj){
-    if (resultObj.status === "ok"){
-        commentsArray = resultObj.data;
-        showComments(commentsArray);
+document.addEventListener("DOMContentLoaded", function (e) {
+    document.getElementById("comments").innerHTML = "";
+    getJSONData(comments_url).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            commentsArray = resultObj.data;
+            showComments(commentsArray);
         }
-}    
-)
+    }
+    )
 })
 
 //Función que muestra comentarios 
 
-function showComments(array){
+function showComments(array) {
     let htmlContentToAppend = "";
     let estrellas = "";
     htmlContentToAppend += "<h3> Comentarios </h3>"
 
     for (let i = 0; i < array.length; i++) {
-        let product_comment  = array[i];
-    
-            estrellas= "";
-            for(let i= 0; i < (product_comment.score); i++) {
-               estrellas +=  ` <span class="fa fa-star checked"> </span> ` 
-            }
+        let product_comment = array[i];
 
-            for(let i=0; i<(5 - (product_comment.score)); i++){
-                estrellas += ` <span class="fa fa-star "> </span> ` 
-            }
-   
+        estrellas = "";
+        for (let i = 0; i < (product_comment.score); i++) {
+            estrellas += ` <span class="fa fa-star checked"> </span> `
+        }
+
+        for (let i = 0; i < (5 - (product_comment.score)); i++) {
+            estrellas += ` <span class="fa fa-star "> </span> `
+        }
+
         htmlContentToAppend += ` 
+        <div class="container ">
         <div class="d-grid gap-3"> 
         <div class="p-2 bg-light border">
 
-        <p> <strong> `+ product_comment.user  + `</strong> ` + " - " + product_comment.dateTime +  estrellas + `</p> 
+        <p> <strong> `+ product_comment.user + `</strong> ` + " - " + product_comment.dateTime + estrellas + `</p> 
 
-        <p> `+ product_comment.description +`</p>
+        <p> `+ product_comment.description + `</p>
         </div>
         
         </div>
+        </div>
              
-        ` 
+        `
         document.getElementById("comments").innerHTML = htmlContentToAppend;
-}
     }
+}
 
 //Agregar un nuevo comentario
 
-if(localStorage.getItem("comentario")){
+if (localStorage.getItem("comentario")) {
     array = JSON.parse(localStorage.getItem("comentario"));
     array.forEach((comentario) => {
-        htmlContentToAppend = ""; 
-        htmlContentToAppend += 
-        `
+        htmlContentToAppend = "";
+        htmlContentToAppend +=
+            ` <div class="container"> 
         <p>${comentario}</p>
+        </div>
            `;
-           document.getElementById("comments").innerHTML = htmlContentToAppend;
+        document.getElementById("comments").innerHTML = htmlContentToAppend;
     });
 }
 
 //Agrega elementos al cuadro si y solo si hay el cuadro de texto no está vacío
 
 enviar.addEventListener("click", () => {
-  
-    if (comentario.value == "" ) {
-        return ;
-    } else{
-    
-    // este bloque transforma el valor de la puntuación en estrellas
+
+    if (comentario.value == "") {
+        return;
+    } else {
+
+        // este bloque transforma el valor de la puntuación en estrellas
         let punt = "";
-    for(let i= 0; i < (puntuacion.value); i++) {
-        punt +=  ` <span class="fa fa-star checked"> </span> ` 
-     }
+        for (let i = 0; i < (puntuacion.value); i++) {
+            punt += ` <span class="fa fa-star checked"> </span> `
+        }
 
-     for(let i=0; i<(5 - (puntuacion.value)); i++){
-         punt += ` <span class="fa fa-star "> </span> ` 
-     }
-    // Finaliza bloque de estrellas
+        for (let i = 0; i < (5 - (puntuacion.value)); i++) {
+            punt += ` <span class="fa fa-star "> </span> `
+        }
+        // Finaliza bloque de estrellas
 
-    array.push(comentario.value);
-    localStorage.setItem("comentario", JSON.stringify(array));
-    if (comentario.value) {
-        let item = `<li class="w-100">
-                <p><strong> ${email} </strong>${ "-"} ${now} ${punt}</p>
+        array.push(comentario.value);
+        localStorage.setItem("comentario", JSON.stringify(array));
+        if (comentario.value) {
+            let item = `<li class="w-100">
+                <p><strong> ${email} </strong>${"-"} ${now} ${punt}</p>
                 <p>${comentario.value} </p>
             </li>
             `;
 
-        contenedor.innerHTML += item;
-      } 
-      comentario.value="";
-  
+            contenedor.innerHTML += item;
+        }
+        comentario.value = "";
+
     }
 });
 
@@ -179,35 +182,41 @@ let relatedArray = [];
 
 
 
-document.addEventListener("DOMContentLoaded", function(e){
-    document.getElementById("related-products").innerHTML= "";
-getJSONData(product_url).then(function(resultObj){
-    if (resultObj.status === "ok"){
-        relatedArray = resultObj.data;
-        showRelated(relatedArray);
+document.addEventListener("DOMContentLoaded", function (e) {
+    document.getElementById("related-products").innerHTML = "";
+    getJSONData(product_url).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            relatedArray = resultObj.data;
+            showRelated(relatedArray);
         }
-    }    
-)
+    }
+    )
 })
 
-function showRelated (array){
-    let htmlContentToAppend = ""; 
+function showRelated(array) {
+    let htmlContentToAppend = "";
 
-        
+
     htmlContentToAppend += ` 
-    
+    <div class="container"> 
     <hr>
-    <h3> Productos relacionados </h3> `  
+    <h3> Productos relacionados </h3> `
 
-    for(let i=0; i< array.relatedProducts.length; i++){
+    for (let i = 0; i < array.relatedProducts.length; i++) {
         let relatedProducts = array.relatedProducts[i];
 
-        
+
 
         htmlContentToAppend += ` 
         <div onclick="setCatID(${relatedProducts.id})" > 
-        <img src= `+ relatedProducts.image + ` class = "w-25 float-none" >  <p> `  + relatedProducts.name  + `  </p> `  
-        
+            <div class="row"> 
+                <div class= "col">
+                    <img src= `+ relatedProducts.image + ` class = "w-25" >  <p> ` + relatedProducts.name + `  </p> 
+                </div>
+            </div>
+        </div>
+        `
+
         document.getElementById("related-products").innerHTML = htmlContentToAppend;
 
 
@@ -216,7 +225,7 @@ function showRelated (array){
 
 // Función para que al momento de hacer click en cada producto se guarde su id en local storage
 
-function setCatID(id){
+function setCatID(id) {
     localStorage.setItem("productID", id)
     window.location = "product-info.html"
 }
